1562fbf7359cec1a40286cf95d2f0513b5bb5685
watari <watari.mailbox@gmail.com>
2018-12-07 15:11:21 +0200

    Added `@codingStandardsIgnoreStart` and `codingStandardsIgnoreEnd` from PHP CodeSniffer to globally ignored annotations list.

--- lib/Doctrine/Common/Annotations/AnnotationReader.php
+++ lib/Doctrine/Common/Annotations/AnnotationReader.php
@@ -103,7 +103,9 @@ class AnnotationReader implements Reader
         // Symfony 3.3 Cache Adapter
         'experimental' => true,
         // Slevomat Coding Standard
-        'phpcsSuppress' => true
+        'phpcsSuppress' => true,
+        // PHP CodeSniffer
+        'codingStandardsIgnoreStart' => true, 'codingStandardsIgnoreEnd' => true
     );
 
     /**
